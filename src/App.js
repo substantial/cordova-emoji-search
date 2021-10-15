6e25734f5dfbe39e40bc6a9480714d95c5e2fef5
Cassie Koomjian <cassie@substantial.com>
2021-10-14 17:06:29 -0700

    Emoji search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import Header from './Header'
+import SearchInput from './SearchInput';
+import SearchResults from './SearchResults';
+import searchEmoji from './search/searchEmoji';
+
+const MAX_SEARCH_RESULTS = 20
 
 function App() {
+  const [searchResults, setSearchResults] = useState(searchEmoji('', MAX_SEARCH_RESULTS))
+
+  const handleSearchChange = searchText => {
+    setSearchResults(searchEmoji(searchText, MAX_SEARCH_RESULTS))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <SearchInput onChangeText={handleSearchChange} />
+      <SearchResults results={searchResults} />
     </div>
   );
 }
